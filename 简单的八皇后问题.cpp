85791131bb1157ffe8e5104594cf0bd070e459ca
zhouzhou012 <61001692+zhouzhou012@users.noreply.github.com>
2020-06-11 22:31:31 +0800

    Add files via upload
    八皇后问题是一个以国际象棋为背景的问题：如何能够在 8*8 的国际象棋棋盘上放置八个皇后，使得任何一个皇后都无法直接吃掉其他的皇后。为了达到此目的，任两个皇后都不能处于同一条横行、纵行或斜线上。
    假如一棋盘已经正确放置了七个皇后，编写程序求解最后一个皇后的放置位置。

---
+++ 简单的八皇后问题.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i,i1,m,n;
+	int sum=0;
+	char s[9];
+	int c[7];
+	cin.get(s,9);// 输入字符串s[9] 3681*752  1327246*
+	for(i=0,i1=0;i<8;i++)
+	{
+		if(s[i]>='0'&&s[i]<='9')
+		  { 
+		    c[i1]=s[i];
+			i1++;
+		  }
+	    else 
+		 m=i+1;//确定未放置棋子的行m=5;c[]存放数字表示已经放置棋子的列   
+	 }
+		for(i=0;i<7;i++)
+			sum=sum+c[i];
+	      n=36+336-sum;
+	      int find=1;
+	      for(i=0;i<7;i++)
+		  {
+		  if((c[i]-48)==(i+1))// 列数等于行数 
+	     {
+		  find=0;
+		  break;
+	     }
+	      } 
+	      if(find==0&&m==n)
+          cout<<"No Answer"<<endl;
+          else
+          cout<<n;	
+          return 0;
+}
+    
+		
+	 
+
